dbaa7551962f6ad59f3e55be9955b532182dbaf9
Tiryaki <Chen.Tiryaki@kcc.com>
2020-09-12 19:51:53 -0400

    final commit

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -3,103 +3,98 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// select the area to insert graph
 var svg = d3.select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-  var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  d3.csv("./assets/data/data.csv").then(function(censusData) {
+// read data from csv
+d3.csv("./assets/data/data.csv").then(function (censusData) {
 
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
-    censusData.forEach(function(data) {
-      data.poverty = +data.poverty;
-      data.healthcare = +data.healthcare;
+    // Parse data as numbers
+    censusData.forEach(function (data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
     });
-console.log(censusData);
+    console.log(censusData);
 
+    // create scales
     var xLinearScale = d3.scaleLinear()
-    .domain([8, d3.max(censusData, d => d.poverty)])
-    .range([0, width]);
+        .domain([8, d3.max(censusData, d => d.poverty)])
+        .range([0, width]);
 
-  var yLinearScale = d3.scaleLinear()
-    .domain([4, d3.max(censusData, d => d.healthcare)])
-    .range([height, 0]);   
-    // Step 3: Create axis functions
-    // ==============================
+    var yLinearScale = d3.scaleLinear()
+        .domain([4, d3.max(censusData, d => d.healthcare)])
+        .range([height, 0]);
+
+    // create axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-       // Step 4: Append Axes to the chart
-    // ==============================
+    // append axis to chart
     chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
 
     chartGroup.append("g")
-      .call(leftAxis);
+        .call(leftAxis);
 
-          // Step 5: Create Circles
-    // ==============================
-    // var circlesGroup = chartGroup.selectAll("circle")
+    // create circles
     chartGroup.selectAll("circle")
-    .data(censusData)
-    .enter()
-    .append("circle")
-    .classed("stateCircle",true)
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "15")
-    .merge(chartGroup)
-    // .text(function(d) { return d.abbr})
-    // .classed("stateText", true);
-    // .html(function(d) {return `<text>${d.abbr}</text>`});
-    // .classed("stateText", true);
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .classed("stateCircle", true)
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .merge(chartGroup)
 
-    // var textGroup = chartGroup.append("g")
+    // append state to circles
     chartGroup.append("g")
-    .selectAll("text")
-    .data(censusData)
-    .enter()
-    .append("text")
-    .classed("stateText", true)
-    .attr("x", d => xLinearScale(d.poverty))
-    .attr("y", d => yLinearScale(d.healthcare))
-    // .merge(chartGroup)
-    // chartGroup.append("text")
-    // .attr("dx", function(d){return -20})
-    .text(function(d) { return d.abbr})
-    // .classed("stateText", true);
-    
+        .selectAll("text")
+        .data(censusData)
+        .enter()
+        .append("text")
+        .classed("stateText", true)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .text(function (d) { return d.abbr })
+
+
+
     // Create axes labels
     chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .attr("class", "axisText")
-      .text("Lacks Healthcare (%)")
-      .classed("active",true);
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)")
+        .classed("active", true);
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("In Poverty(%)")
-      .classed("active",true);
-  })
-  
-  .catch(function(error) {
-    console.log(error);
-  });
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty(%)")
+        .classed("active", true);
+
+
+})
+    // catch errors
+    .catch(function (error) {
+        console.log(error);
+    });
